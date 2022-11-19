ca1e7a4edfe1ba467934bfa29d71ee4be26fc9a5
Ras96 <asymptote.k.k@gmail.com>
2022-11-19 09:39:00 +0900

    :sparkles: calculate typical values

---
+++ calculate_typical.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("dist/analyze_fixation.csv")
+df = df.loc[:, ["Time", "Media", "Count", "TotalTime", "TotalDistance"]].groupby(["Time", "Media"])
+df.mean().round(2).to_csv("dist/calculated_typical_mean.csv")
+df.median().round(2).to_csv("dist/calculated_typical_median.csv")
+df.std().round(2).to_csv("dist/calculated_typical_std.csv")
+
+
