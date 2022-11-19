816eafa0d4b82da1448039d3c7901646b6098776
Ras96 <asymptote.k.k@gmail.com>
2022-11-19 09:41:55 +0900

    :boom: dont sort

--- calculate_typical.py
+++ calculate_typical.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("dist/analyze_fixation.csv")
-df = df.loc[:, ["Time", "Media", "Count", "TotalTime", "TotalDistance"]].groupby(["Time", "Media"])
+df = df.loc[:, ["Time", "Media", "Count", "TotalTime", "TotalDistance"]].groupby(["Time", "Media"], sort=False)
 df.mean().round(2).to_csv("dist/calculated_typical_mean.csv")
 df.median().round(2).to_csv("dist/calculated_typical_median.csv")
 df.std().round(2).to_csv("dist/calculated_typical_std.csv")
