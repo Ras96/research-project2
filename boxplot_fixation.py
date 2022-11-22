3ffc7f699d4431f33b497f8ed04564886255b028
Ras96 <asymptote.k.k@gmail.com>
2022-11-23 03:46:13 +0900

    :memo: add title

--- boxplot_fixation.py
+++ boxplot_fixation.py
@@ -1,13 +1,17 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv("dist/analyze_fixation.csv")
 df = df.loc[:, ["Time", "Media", "Count", "TotalTime", "TotalDistance"]]
-df.boxplot(by=["Media", "Time"], column=["Count"], grid=False, showmeans=True, whis=1.5).get_figure().savefig(
-    "dist/boxplot_fixation_count.png"
-)
-df.boxplot(by=["Media", "Time"], column=["TotalTime"], grid=False, showmeans=True, whis=1.5).get_figure().savefig(
-    "dist/boxplot_fixation_totaltime.png"
-)
-df.boxplot(by=["Media", "Time"], column=["TotalDistance"], grid=False, showmeans=True, whis=1.5).get_figure().savefig(
-    "dist/boxplot_fixation_totaldistance.png"
-)
+
+df.boxplot(by=["Media", "Time"], column=["Count"], grid=False, showmeans=True, whis=1.5)
+plt.title("Fixation Count")
+plt.savefig("dist/boxplot_fixation_count.png")
+
+df.boxplot(by=["Media", "Time"], column=["TotalTime"], grid=False, showmeans=True, whis=1.5)
+plt.title("Fixation Total Time")
+plt.savefig("dist/boxplot_fixation_totaltime.png")
+
+df.boxplot(by=["Media", "Time"], column=["TotalDistance"], grid=False, showmeans=True, whis=1.5)
+plt.title("Fixation Total Distance")
+plt.savefig("dist/boxplot_fixation_totaldistance.png")
